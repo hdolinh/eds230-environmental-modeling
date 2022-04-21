c88749744227f583bed77c135f5a8e5c58bdbef8
hdolinh <halina@bren.ucsb.edu>
2022-04-21 09:27:49 -0700

    added roxygen to cat function

--- hw/functions/cat.R
+++ hw/functions/cat.R
@@ -1,3 +1,16 @@
+#' Compute atmospheric conductance
+#'
+#' @param zm the height that windspeed is measured at; must be higher than the vegetation (h) and is typically measured 200 cm above
+#' @param h vegetation height in cm
+#' @param v windspeed in cm/s
+#' @param kd constant at 0.7
+#' @param k0 constant at 0.1
+#'
+#' @return
+#' @export
+#'
+#' @examples cat(zm = 400, h = 200, v = 5)
+#' 
 cat <- function(zm, h, v, kd = 0.7, k0 = 0.1){
   
   # creating zd and z0
